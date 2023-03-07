a6e999a34515053613e58dc2c64b44b83c6da454
artwoojin <artwoojin@gmail.com>
2023-03-07 14:16:51 +0900

    Project Init

---
+++ server/src/main/java/com/seb42/main30/seb42_main_030/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.seb42.main30.seb42_main_030;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/seb42/main30/seb42_main_030/Seb42Main030ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seb42.main30.seb42_main_030;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seb42Main030ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
