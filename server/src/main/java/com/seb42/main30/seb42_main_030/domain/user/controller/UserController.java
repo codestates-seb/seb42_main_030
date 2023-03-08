c4b503f569d5b29934cdb1225fd51ecdc030260e
artwoojin <artwoojin@gmail.com>
2023-03-08 13:06:15 +0900

    build : BE 초기 세팅 2

---
+++ server/src/main/java/com/seb42/main30/seb42_main_030/domain/user/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.seb42.main30.seb42_main_030.domain.user.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
