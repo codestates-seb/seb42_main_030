38d66a3a996cca6a840801110b11c742a8fb8b8a
artwoojin <artwoojin@gmail.com>
2023-03-10 09:43:27 +0900

    feat : userService 일부 구현

--- server/src/main/java/com/seb42/main30/seb42_main_030/domain/user/service/UserService.java
+++ server/src/main/java/com/seb42/main30/seb42_main_030/domain/user/service/UserService.java
@@ -1,4 +1,40 @@
 package com.seb42.main30.seb42_main_030.domain.user.service;
 
+import com.seb42.main30.seb42_main_030.domain.user.entity.User;
+import com.seb42.main30.seb42_main_030.domain.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    // todo (1) user 등록(소셜 회원 가입)
+
+    // (2) user 정보 수정
+    public User updateUser(User user) {
+        User findUser = findVerifiedUser(user.getUserId());
+
+        Optional.ofNullable(user.getNickname())
+                .ifPresent(name -> findUser.setNickname(name));
+        Optional.ofNullable(user.getImageUrl())
+                .ifPresent(url -> findUser.setImageUrl(url));
+
+        // 회원정보 업데이트
+        return userRepository.save(findUser);
+    }
+
+    // todo (3) user 정보 조회
+    public User findUser(long userId) {
+        return findVerifiedUser(userId);
+    }
+
+    // todo (4) 회원 탈퇴
+    public void deleteUser(long userId) {
+
+    }
+
+
 }
