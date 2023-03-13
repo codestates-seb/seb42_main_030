eca0e725a99b769210abaaabe52802ce4724d58b
Kimlabe <labe1827@gmail.com>
2023-03-13 13:13:29 +0900

    feat : 게시물 기본 클래스 추가

---
+++ server/src/main/java/com/seb42/main30/seb42_main_030/domain/diary/dto/DiaryPatchDto.java
@@ -0,0 +1,30 @@
+package com.seb42.main30.seb42_main_030.domain.diary.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DiaryPatchDto {
+    @Positive
+    private long userId;
+
+    @Positive
+    private long diaryId;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String body;
+
+}
