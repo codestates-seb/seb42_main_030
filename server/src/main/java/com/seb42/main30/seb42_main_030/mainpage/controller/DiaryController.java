392388f0484cb2b48abc9368084efdb8db0e8944
p03695 <p036950@gmail.com>
2023-03-16 17:10:45 +0900

    pagenation

--- server/src/main/java/com/seb42/main30/seb42_main_030/mainpage/controller/DiaryController.java
+++ server/src/main/java/com/seb42/main30/seb42_main_030/mainpage/controller/DiaryController.java
@@ -1,6 +1,8 @@
 package com.seb42.main30.seb42_main_030.mainpage.controller;
 
 import com.seb42.main30.seb42_main_030.mainpage.entity.Diary;
+import com.seb42.main30.seb42_main_030.mainpage.service.DiaryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -11,6 +13,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DiaryController {
+    @Autowired
+    private DiaryService diaryService;
+
+    //게시글 리스트 페이지네이션(한 페이지에 12게시글씩)
     @GetMapping("/diary/list")
     public String diaryList(Model model, @PageableDefault(page = 0, size = 12, sort = "diaryId", direction = Sort.Direction.DESC) Pageable pageable){
 
@@ -28,5 +34,12 @@ public class DiaryController {
 
         model.addAttribute("list", diaryService.diaryList(pageable));
         return "diaryList";
+    }
+
+    //게시글로 이동
+    @GetMapping("/diary/view") //?id=번호
+    public String diaryView(Model model, Long id){
+        model.addAttribute("diary", diaryService.diaryView(id));
+        return "diaryView";
     }
 }
--- server/src/main/java/com/seb42/main30/seb42_main_030/mainpage/repository/DiaryRepository.java
+++ server/src/main/java/com/seb42/main30/seb42_main_030/mainpage/repository/DiaryRepository.java
@@ -1,2 +1,7 @@
-package com.seb42.main30.seb42_main_030.mainpage.repository;public interface mainpageRepository {
+package com.seb42.main30.seb42_main_030.mainpage.repository;
+
+import com.seb42.main30.seb42_main_030.mainpage.entity.Diary;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiaryRepository extends JpaRepository<Diary, Long> {
 }
