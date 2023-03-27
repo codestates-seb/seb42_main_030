43b4f79e11708e38c5c02a69c66840c309ffff2b
p03695 <p036950@gmail.com>
2023-03-27 18:30:01 +0900

    refact: 수정

--- server/src/main/java/com/seb42/main30/seb42_main_030/auth/controller/OauthController.java
+++ server/src/main/java/com/seb42/main30/seb42_main_030/auth/controller/OauthController.java
@@ -1,6 +1,8 @@
 package com.seb42.main30.seb42_main_030.auth.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -19,7 +21,18 @@ import java.util.Base64;
 @RequestMapping("/callback")
 @Validated
 @RequiredArgsConstructor
+@PropertySource("classpath:application.yml")
 public class OauthController {
+    @Value("${spring.security.oauth2.client.registration.spotify.clientId}") //application.yml에서 clientId 가져오기
+    String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.spotify.clientSecret}") //application.yml에서 clientSecret 가져오기
+    String clientSecret;
+
+    @Value("${spring.security.oauth2.client.registration.spotify.redirect_uri}")
+    String redirectUri;
+
+
     //URL의 쿼리에서 Authorization code 받아오기
     @ResponseBody
     @GetMapping("/login")
@@ -33,13 +46,13 @@ public class OauthController {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-type", "application/x-www-form-urlencoded");
         //Authorization: Basic <base64 encoded client_id:client_secret>
-        headers.add("Authorization", "Basic "+ base64("a74acd41a54442fba564ef852d9a43b0:3ef5274217594784b2fb16e14f3ddf62"));
+        headers.add("Authorization", "Basic "+ base64(clientId+":"+clientSecret));
 
         //HttpBody에 데이터 담기
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "authorization_code");
         params.add("code", code);
-        params.add("redirect_uri", "http://localhost:8080/callback/login");    //application.yml의 redirect-uri와 동일한 uri
+        params.add("redirect_uri", redirectUri);    //application.yml의 redirect-uri와 동일한 uri
 
         RestTemplate restTemplate = new RestTemplate();
         //HttpHeader와 HttpBody를 하나로 묶기
