9db636e4209df00bfcf506013095b86e03b6c52e
handaehee93 <hdh8990@naver.com>
2023-03-28 13:30:16 +0900

    다이어리 수정 페이지 patch요청 구현

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,8 @@ import Signup from "./pages/Signup";
 import { Routes, Route } from "react-router-dom";
 import { createGlobalStyle } from "styled-components";
 import Addplaylist from "./components/NewDiary/Addplaylist";
+import EditDetail from './components/EditDiary/EditList';
+import EditDiary from './pages/EditDiary';
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -32,6 +34,7 @@ function App() {
         <Route path='/Login' element={<Login />} />
         <Route path='/Signup' element={<Signup />} />
         <Route path='/DetailDiary/:diaryId' element={<DetailDiary />} />
+        <Route path='/EditDiary/:diaryId' element={<EditDiary />} />
       </Routes>
     </div>
   );
---
+++ client/src/pages/EditDiary.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import EditMain from '../components/EditDiary/EditMain'
+export default function EditDiary() {
+  return (
+    <div>
+      <EditMain />
+    </div>
+  )
+}
