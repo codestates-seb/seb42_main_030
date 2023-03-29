18fe32d1252a6c717661637dc39885cdd44cecfd
kimdonggu42 <kdonggoo44@gmail.com>
2023-03-29 11:51:37 +0900

    deploy : env 환경변수 설정

--- client/src/util/API.tsx
+++ client/src/util/API.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://ec2-15-164-230-157.ap-northeast-2.compute.amazonaws.com:8080";
-const TOKEN = `eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJVU0VSIl0sInVzZXJuYW1lIjoibGltMDFAbmF2ZXIuY29tIiwic3ViIjoibGltMDFAbmF2ZXIuY29tIiwiaWF0IjoxNjgwMDEzNzk1LCJleHAiOjE2ODA2MTM3OTV9.body8FyruMs9D2irxq4RAC1iuDaDxKAeY8ge5saoPKHizvPNijngWWLF_VQVVEUA-W72xJUIdYzqPe8NId52ug`;
+const BASE_URL = process.env.REACT_APP_API_URL;
+const TOKEN = process.env.REACT_APP_USER_TOKEN;
 
 export const BASE_API = axios.create({
   baseURL: BASE_URL,
